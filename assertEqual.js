232bbafddfd5214d0f78386caa6bc1976620329d
JRam2022 <jasonram778@gmail.com>
2022-04-04 16:57:57 -0700

    assertEquals Function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("☠️  ☠️  ☠️" + "  Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("👌 👌 👌" + " Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(3, 9);
