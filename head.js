fdebae64b270a32ea28e217c707f3149933ff457
JRam2022 <jasonram778@gmail.com>
2022-04-04 17:58:26 -0700

    Head function

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👌 👌 👌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☠️  ☠️  ☠️   Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const head = function(array) {
+  return array[0];
+
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
