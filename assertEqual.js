90ab1a784f80119337ae596ceca2c339d3c0ab0f
JRam2022 <jasonram778@gmail.com>
2022-04-04 17:09:48 -0700

    Added temporate literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("☠️  ☠️  ☠️" + "  Assertion Passed: " + actual + " === " + expected);
+    console.log(`☠️  ☠️  ☠️  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("👌 👌 👌" + " Assertion Failed: " + actual + " !== " + expected);
+    console.log(`👌 👌 👌 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
