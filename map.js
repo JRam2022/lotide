f56cd5287e896869dd48362582d56962ccbf3a85
JRam2022 <jasonram778@gmail.com>
2022-04-07 15:27:30 -0700

    I need to start commiting more

---
+++ map.js
@@ -0,0 +1,14 @@
+const map = function() {
+  const reults = [];
+  return results1;
+
+};
+
+
+
+
+const words = ["ground", "control", "to", "major", "tome"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
