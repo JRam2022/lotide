a6fed8c422642ca2239c6e4c72441f29e9b636f2
JRam2022 <jasonram778@gmail.com>
2022-04-06 17:10:43 -0700

    Function for letters positions

---
+++ lettersPositions.js
@@ -0,0 +1,23 @@
+const letterPositions = function(sentence) {
+  // letter as key and number as value
+  let results = {};
+  //loop through the string
+  for (let i = 0; i < sentence.length; i++) {
+    //console.log(letter);
+    //if theres a space skip it
+    if (sentence[i] === ' ') {
+      continue;
+    }
+    
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    }  else {
+      let tempArray = [];
+      tempArray.push(i);
+      results[sentence[i]] = tempArray;
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions('my name is jason'));
