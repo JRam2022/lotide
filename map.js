9b2df77738ff4d78f7f0933234dc11b585cb35c3
JRam2022 <jasonram778@gmail.com>
2022-04-07 15:36:29 -0700

    added functionality

--- map.js
+++ map.js
@@ -1,6 +1,18 @@
-const map = function() {
-  const reults = [];
-  return results1;
+const map = function(array, callback) {
+
+  //console.log('array: ', array);
+  //console.log('callback: ', callback);
+
+
+  const results = [];
+  
+  for (let item of array) {
+    console.log('item Before: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
+
+  return results;
 
 };
 
