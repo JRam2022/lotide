ddff5a47efb04ac6d5021c1763191bdf101d995f
JRam2022 <jasonram778@gmail.com>
2022-04-11 16:45:31 -0700

    Added more functions to index

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 const countLetters = require('./countLetters')
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+
 
 module.exports = {
   head: head,
   tail: tail,
   middle: middle,
   countLetters: countLetters,
-  
+  eqArrays: eqArrays,
+  eqObjects: eqObjects
+
+
 };
