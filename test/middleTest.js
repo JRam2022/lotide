896bd907059a3bb331b2c55c85eafcbeb71f754f
JRam2022 <jasonram778@gmail.com>
2022-04-11 15:37:40 -0700

    Added mocha/chai tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 const middle = require('../middle');
 
-assertArraysEqual((middle([1, 2]), [])); // => []
-assertArraysEqual((middle([1, 2, 3, 40, 50, 6, 7, 8]), [40,50])); // => [40, 50]
-assertArraysEqual((middle([1, 2, 3, 4, 50, 6, 7, 8 ,9]), [50])); // => [50]
+describe('#middle', () => {
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  })
+
+  it("returns [40,50] for 1, 2, 3, 40, 50, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 40, 50, 6, 7, 8]), [40,50]);
+  })
+
+  it("returns [50] for [1, 2, 3, 4, 50, 6, 7, 8 ,9]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 50, 6, 7, 8 ,9]), [50]);
+  })
+
+});
