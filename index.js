77e83c310ec74fe16e76118a6bcb231bf23aa10d
JRam2022 <jasonram778@gmail.com>
2022-04-11 18:16:08 -0700

    Added functons

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const middle = require('./middle');
 const countLetters = require('./countLetters')
 const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const lettersPositions = require('./lettersPositions');
+const map = require('./map');
+const findKey = require('./findKey');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual')
+
 
 
 module.exports = {
@@ -12,7 +20,13 @@ module.exports = {
   middle: middle,
   countLetters: countLetters,
   eqArrays: eqArrays,
-  eqObjects: eqObjects
-
+  eqObjects: eqObjects,
+  findKeyByValue: findKeyByValue,
+  lettersPositions: lettersPositions,
+  map: map,
+  findKey: findKey,
+  assertObjectsEqual: assertObjectsEqual,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual
 
 };
