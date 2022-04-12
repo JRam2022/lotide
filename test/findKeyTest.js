a3ee1eb77028a2f1929b336534c314777034149c
JRam2022 <jasonram778@gmail.com>
2022-04-11 17:29:48 -0700

    Added mocha/chai tests

---
+++ test/findKeyTest.js
@@ -0,0 +1,41 @@
+const assert = require('chai').assert;
+
+const findKey = require('../findKey');
+
+describe("#findKey", () => {
+
+  it("returns 'noma' for object", () => {
+    assert.deepEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    }, x => x.stars === 2), "noma");
+  });
+
+  it("returns 'Akaleri' for object", () => {
+    assert.deepEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    }, x => x.stars === 3), "Akaleri");
+  });
+
+  it("returns 'Blue Hill' for object", () => {
+    assert.deepEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    }, x => x.stars === 1), "Blue Hill");
+  });
+
+  
+});
